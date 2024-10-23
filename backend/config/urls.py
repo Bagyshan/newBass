2743472871afa48b6b2b6adadf13f2117e0b4f6d
Bagyshan <Bagishan01@gmail.com>
2024-10-24 00:07:34 +0600

    hot fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -38,4 +40,4 @@ urlpatterns = [
     path('api/', include('src.posts.urls')), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
